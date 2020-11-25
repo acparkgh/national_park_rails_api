dd35811ba4b66fab120438c44f10bacae1390300
acparkgh <acpark123@gmail.com>
2020-11-25 17:02:29 -0500

    added user for comment

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -3,15 +3,27 @@ class Api::V1::ParksController < ApplicationController
   def index
     @parks = Park.all
     render json: @parks.to_json( include: { mytrips: {
-                                   include: [:comments] } }
+                                   include: [ :user, :comments] } }
                                )
   end
 
   def show
     @park = Park.find_by(id: params[:id])
     render json: @park.to_json( include: { mytrips: {
-                                  include: [:comments] } } 
+                                  include: [ :user, :comments ] } } 
                               )
   end
+
+  # def show ...example
+  #   @park = Park.find_by(id: params[:id])
+  #   render json: @park.to_json( include: { mytrips: {
+  #                                 include: [:comments] } } 
+  #                             )
+  # end
+
+  # def index example
+  #   users = User.all
+  #   render json: users, only: [:id, :username, :password, :address], include: [projects: [:supplies]]
+  # end
 
 end
