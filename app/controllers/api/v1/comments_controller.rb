74a448ef35d01ad3b962b7b7b6bf3ac9245aa018
acparkgh <acpark123@gmail.com>
2020-12-01 18:27:12 -0500

    add comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -10,4 +10,33 @@ class Api::V1::CommentsController < ApplicationController
     render json: @comment.to_json
   end
 
+  def create
+    # byebug
+    @comment = Comment.new
+    @comment.mytrip_id = params[:mytrip_id]
+    @comment.comment = params[:comment]
+    @comment.save
+
+    render json: @comment
+  end
+
+
+  # def create
+  #   # byebug
+  #   @comment = Comment.find_by(id: params[:id])
+  #   @comment = Comment.create(
+  #     comment_params(
+  #       :mytrip_id, 
+  #       :comment
+  #     )
+  #   )
+  #   render json: @comment
+  # end
+
+  # private
+
+  #   def comment_params(*args)
+  #     params.require(:comment).permit(*args)
+  #   end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users.to_json
+    render json: @users.to_json(include: [:mytrips])
   end
 
   def show
