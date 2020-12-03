638c4d250da1fb51acc9123b942021ba0a623186
acparkgh <acpark123@gmail.com>
2020-12-03 03:50:10 -0500

    continue on Styling

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ActivitiesController < ApplicationController
+
+  def index
+    @activities = Activity.all
+    render json: @activities.to_json
+  end
+
+  def show
+    @activity = Activity.find_by(id: params[:id])
+    render json: @Activity.to_json
+  end
+
 end
--- app/controllers/api/v1/parkactivities_controller.rb
+++ app/controllers/api/v1/parkactivities_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::ParkactivitiesController < ApplicationController
+
+  def index
+    @parkactivities = Parkactivity.all
+    render json: @parkactivities.to_json(include: [:activities])
+  end
+
+  def show
+    @parkactivity = parkactivity.find_by(id: params[:id])
+    render json: @parkactivity.to_json(include: [:activities])
+  end
+
+
 end
